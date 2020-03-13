e8e3ea326d21351770dcf864a8e33cd5b453102d
Kun Zou <kun.zou@cwbnationalleasing.com>
2020-03-13 10:34:30 -0500

    Email service in progress Added mocks for testing

--- src/main/java/kunzou/me/scheduler/handlers/MessagingExceptionHandler.java
+++ src/main/java/kunzou/me/scheduler/handlers/MessagingExceptionHandler.java
@@ -2,6 +2,8 @@ package kunzou.me.scheduler.handlers;
 
 import javax.mail.MessagingException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -10,13 +12,16 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.client.RestClientResponseException;
 
+import kunzou.me.scheduler.services.EmailService;
+
 @RestControllerAdvice(annotations = RestController.class)
 public class MessagingExceptionHandler {
-
+	private static final Logger logger = LoggerFactory.getLogger(EmailService.class);
 	@ExceptionHandler(MessagingException.class)
 	@ResponseStatus(HttpStatus.SERVICE_UNAVAILABLE)
 	@ResponseBody
 	public String badRequestExceptionHandler(RestClientResponseException e) {
+		logger.error(e.getMessage(), e);
 		return "Failed to send email. Please contact the owner";
 	}
 }
--- src/main/java/kunzou/me/scheduler/services/EmailService.java
+++ src/main/java/kunzou/me/scheduler/services/EmailService.java
@@ -2,6 +2,8 @@ package kunzou.me.scheduler.services;
 
 import kunzou.me.scheduler.domains.Appointment;
 import kunzou.me.scheduler.domains.Schedule;
+import kunzou.me.scheduler.exception.EmailServiceException;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.mongodb.core.MongoTemplate;
@@ -10,36 +12,86 @@ import org.springframework.stereotype.Service;
 import javax.mail.*;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
+
+import java.time.format.DateTimeFormatter;
+import java.util.Optional;
 import java.util.Properties;
 
 @Service
 public class EmailService {
   private static final Logger logger = LoggerFactory.getLogger(EmailService.class);
   private MongoTemplate mongoTemplate;
+  private static final String EMAIL_SUBJECT = "New appointment: %s to %s, %s";
+  private static final String GUEST_EMAIL_BODY =
+      "Hello %s,\n" +
+          "\t\tYour appointment with %s at %s is pending. You will be notified once it is approved. Thank you.\n\t" +
+          "To review your appoint, please visit %s";
+
+  private static final String USER_EMAIL_BODY = "Guest name: %s\nEmail: %s\nMessage: %s\nTo approve or decline this appointment, please visit: %s";
 
   public EmailService(MongoTemplate mongoTemplate) {
     this.mongoTemplate = mongoTemplate;
   }
 
   public void notifyScheduleOwner(Appointment appointment) throws MessagingException {
-    Transport.send(createMessage(appointment));
+    Transport.send(createEmailToUser(appointment));
   }
 
-  private Message createMessage(Appointment appointment) throws MessagingException {
+  public void notifyGuest(Appointment appointment) throws MessagingException {
+    Transport.send(createEmailToGuest(appointment));
+  }
+
+  Message createEmailToGuest(Appointment appointment) throws MessagingException {
     Message message = createEmailableMessage();
-    String to = mongoTemplate.findById(appointment.getScheduleId(), Schedule.class).getUserEmail();
+    String to = appointment.getGuestEmail();
     message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(to));
-    message.setSubject("New appointment: "+ appointment.getStart() + appointment.getEnd());
-    message.setText(
-      "First name:"+appointment.getGuestFirstName()+"\n"+
-      "Last name:"+appointment.getGuestLastName()+"\n"+
-      "Email:"+appointment.getGuestEmail()+"\n"+
-      "Message:"+appointment.getGuestMessage()+"\n"+
-      "To approve or decline this appointment, please visit" +
-      System.getenv("HOST_URL") + "/calendar/" + appointment.getScheduleId()
-    );
+    message.setSubject(formatEmailSubject(appointment));
+    message.setText(createGuestEmailBody(appointment));
 
     return message;
+  }
+
+  private String createGuestEmailBody(Appointment appointment) {
+    return String.format(GUEST_EMAIL_BODY,
+        appointment.getGuestFirstName(),
+        Optional.ofNullable(mongoTemplate.findById(appointment.getScheduleId(), Schedule.class)).map(Schedule::getName).orElse("us"),
+        appointment.getStart().format(DateTimeFormatter.ofPattern("HH:mm, E, MMM dd, yyyy z")),
+        getCalendarLink(appointment)
+    );
+  }
+
+  private String createUserEmailBody(Appointment appointment) {
+    return String.format(USER_EMAIL_BODY,
+        appointment.getGuestName(),
+        appointment.getGuestEmail(),
+        appointment.getGuestMessage(),
+        getCalendarLink(appointment)
+    );
+  }
+
+  private Message createEmailToUser(Appointment appointment) throws MessagingException {
+    Message message = createEmailableMessage();
+    String to = Optional.ofNullable(mongoTemplate.findById(appointment.getScheduleId(), Schedule.class))
+        .map(Schedule::getUserEmail)
+        .orElseThrow(() -> new EmailServiceException("Cannot find user email"));
+    message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(to));
+    message.setSubject(formatEmailSubject(appointment));
+    message.setText(createUserEmailBody(appointment));
+
+    return message;
+  }
+
+  private String getCalendarLink(Appointment appointment) {
+    return String.join(System.getenv("HOST_URL"), "/calendar/", appointment.getScheduleId());
+  }
+
+  private String formatEmailSubject(Appointment appointment) {
+    DateTimeFormatter timeFormat = DateTimeFormatter.ofPattern("HH:mm");
+    DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("E, MMM dd,yyyy z");
+    return String.format(EMAIL_SUBJECT,
+        appointment.getStart().format(timeFormat),
+        appointment.getEnd().format(timeFormat),
+        appointment.getStart().format(dateFormat));
   }
 
   private Message createEmailableMessage() throws javax.mail.MessagingException {
